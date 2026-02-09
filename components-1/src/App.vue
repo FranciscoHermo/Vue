03e03c1cf99780fd380732e78f2354603a923f1a
Francisco <jvasillo300@gmail.com>
2026-02-09 15:07:20 -0600

    Vue

---
+++ components-1/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import ButtonCounter from './components/ButtonCounter.vue';
+</script>
+
+
+<template>
+  <div>
+    <h1>APP</h1>
+  <ButtonCounter/>
+  <ButtonCounter/>
+  </div>
+  
+  
+</template>
